ded40d244f26be2811d814c0190918ea14eda8ae
Marcus Vinícius Pires <mvpires@gmail.com>
2017-01-03 22:55:33 -0300

    - Further work on some animations and colliders.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,22 +4,26 @@ using System.Collections;
 public class EnemyBehavior : MonoBehaviour
 {
 
-    Animation mAnimation;
+    private Animation mAnimation;
+    private NavMeshAgent mNavMeshAgent;
+    public GameObject mPlayer;
+    private float mAttackDistance;
 
     // Use this for initialization
     void Start()
     {
-
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+        mAnimation = GetComponent<Animation>();
     }
 
     void Awake()
     {
-        mAnimation = GetComponent<Animation>();
-
         if (tag.Equals("Skeleton"))
         {
-            mAnimation["Idle"].layer = 1;
-            mAnimation["Walk"].layer = 1;
+
+            //mAnimation["Idle"].layer = 1;
+            //mAnimation["Walk"].layer = 1;
+            //mAnimation["Attack"].layer = 2;
         }
 
     }
@@ -27,6 +31,27 @@ public class EnemyBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mAttackDistance = Vector3.Distance(gameObject.transform.position, mPlayer.transform.position);
+        mNavMeshAgent.SetDestination(mPlayer.transform.position);
+        mAnimation.CrossFade("Walk");
 
+        if (mAttackDistance < 20.0f)
+        {
+            Attack();
+            mNavMeshAgent.Stop();
+        }
+        else if (mAttackDistance > 20.0f)
+        {
+            mAnimation.CrossFade("Walk");
+            mNavMeshAgent.Resume();
+        }
+
+        
+    }
+
+    void Attack()
+    {
+        mAnimation.CrossFade("Attack");
+        mAnimation["Attack"].speed = 2F;
     }
 }
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class PlayerBehavior : MonoBehaviour {
 
     private Animation mAnimation;
+    public bool mMoveLeft = false;
+    public bool mMoveRight = false;
+    public bool mIsAttacking = false;
+    public bool mIsDefending = false;
+    public float mSpeed = 16f;
+    private CharacterController mCharacterController;
 
     void Awake()
     {
         mAnimation = GetComponent<Animation>();
+        mCharacterController = GetComponent<CharacterController>();
 
-        
+
             mAnimation["Wait"].layer = 1;
             mAnimation["Wait"].wrapMode = WrapMode.Loop;
             mAnimation["Walk"].layer = 1;
@@ -25,6 +32,70 @@ public class PlayerBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if(mMoveLeft)
+        {
+
+            mCharacterController.Move(-Vector3.right * mSpeed * Time.deltaTime);
+            mAnimation.CrossFade("Walk");
+            mAnimation["Walk"].speed = 2F;
+        }
+        else if (mMoveRight)
+        {
+            mCharacterController.Move(Vector3.right * mSpeed * Time.deltaTime);
+            mAnimation.CrossFade("Walk");
+            mAnimation["Walk"].speed = 2F;
+        }
+
+        //if (mIsAttacking)
+        //{
+        //    mAnimation.Blend("Attack");
+        //    mAnimation["Attack"].speed = 2F;
+
+        //}
+
+
+    }
+
+    public void Attack()
+    {
+        mIsAttacking = true;
+    }
+
+    public void Defend()
+    {
+        mIsDefending = true;
+    }
+
+    public void MoveLeft()
+    {
+        mMoveLeft = true;
+    }
+
+    public void MoveRight()
+    {
+        mMoveRight = true;
+    }
+
+    public void RightButtonUp()
+    {
+        mAnimation.CrossFade("Wait");
+        mMoveRight = false;
+    }
+
+    public void LeftButtonUp()
+    {
+        mAnimation.CrossFade("Wait");
+        mMoveLeft = false;
+    }
+
+    public void AttackButtonUp()
+    {
+        mIsAttacking = false;
+    }
+
+    public void DefenseButtonUp()
+    {
+        mIsDefending = false;
+    }
 }
